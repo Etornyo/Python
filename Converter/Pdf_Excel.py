ff790cc6480f33eb3a9b60631a1e22f5375a9901
Zekhz <83181379+Etornyo@users.noreply.github.com>
2023-10-10 17:35:50 +0000

    Work

--- Converter/Pdf_Excel.py
+++ Converter/Pdf_Excel.py
@@ -1,12 +1,20 @@
+# import tabula
+# import pandas as pd
+#
+#
+#
+# # Extract from pdf
+# def extract_from_pdf(pdf_path):
+#     try:
+#         tables = tabula.read_pdf(pdf_path,pages='all',multiple_tables=True)
+#         return tables
+#
+#     except Exception as
+
 import tabula
-import pandas as pd
 
+inp = (r"[pdf name]")
+oup = (r"test.cvs")
 
-
-# Extract from pdf
-def extract_from_pdf(pdf_path):
-    try:
-        tables = tabula.read_pdf(pdf_path,pages='all',multiple_tables=True)
-        return tables
-
-    except Exception as
+df = tabula.read_pdf(input_path=inp, pages="all")
+tabula.convert_into(input_path = inp, output_path=oup, output_format="cvs", pages="all", stream=True)
